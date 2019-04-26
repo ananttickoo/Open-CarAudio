6ad048aa8c5ef810505923a667ee1264870301cd
anant tickoo <ananttickoo@gmail.com>
2019-04-26 14:55:14 +0530

    removing header head and moving to single file.

--- src/bike_play.c
+++ src/bike_play.c
@@ -1,13 +1,4 @@
-#include <wiringPiSPI.h>
-#include <stdio.h>
-#include <wiringPi.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <string.h>
-#include <errno.h>
-#include <pthread.h>
+#include <include.h>
 
 
 int main()
