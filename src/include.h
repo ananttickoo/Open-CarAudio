8194bc6a6c3dec8d8a9e4813d432c63ffaf77256
anant tickoo <ananttickoo@gmail.com>
2019-04-26 14:54:11 +0530

    Just to keep code clean

---
+++ src/include.h
@@ -0,0 +1,10 @@
+#include <wiringPiSPI.h>
+#include <stdio.h>
+#include <wiringPi.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
