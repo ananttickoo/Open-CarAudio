141d955f66ff12f0b908da3e6622498765b4fdd3
anant tickoo <ananttickoo@gmail.com>
2019-04-27 19:02:27 +0530

    fixing build file

--- build/build.sh
+++ build/build.sh
@@ -3,8 +3,8 @@
 #create run if does not exit
 mkdir -p ../run/
 
-LIB="-lwiringpi -lpthreads"
-LIBPATH=" -L/usr/local/include -L/usr/local/lib"
+LIB="-lwiringPi -lpthread"
+LIBPATH="-L/usr/include  -L/usr/local/lib"
 cd ../src
 
 gcc spi.c bike_play.c -o ../run/bike_play $LIBPATH $LIB
