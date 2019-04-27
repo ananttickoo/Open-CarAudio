0bcd5b2f47261a8dc5cc6cc8db81929cfea0d661
anant tickoo <ananttickoo@gmail.com>
2019-04-27 12:29:54 +0530

    version 0.1 buggy

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,8 @@
 mkdir -p ../run/
 
 LIB="-lwiringpi -lpthreads"
-
+LIBPATH=" -I/usr/local/include -L/usr/local/lib"
 cd ../src
 
-gcc spi.c bike_play.c -o ../run/bike_play $LIB
+gcc spi.c bike_play.c -o ../run/bike_play $LIBPATH $LIB
+
