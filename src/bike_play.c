28fd8004fbe284ee9de1f78bc3c8bc0c0c558f1b
anant tickoo <ananttickoo@gmail.com>
2019-04-28 23:45:20 +0530

    debug add

--- src/bike_play.c
+++ src/bike_play.c
@@ -14,8 +14,10 @@
 int main()
 {
 	pthread_t thread_id;
+	printf("starting thread to read from SPI \n");
 	pthread_create(&thread_id, NULL, readAnalog, NULL);
 	sleep(.2);
+	printf("starting thread to read interpert joystick \n");
 	pthread_create(&thread_id, NULL, read_spi_input, NULL);
 	pthread_join(thread_id, NULL);
 	exit(0);
