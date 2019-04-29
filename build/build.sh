98a917f5085ed3899f7b48b2a9aad028655cc25b
anant tickoo <ananttickoo@gmail.com>
2019-04-29 22:50:56 +0530

    edit debug

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,6 @@ LIB="-lwiringPi -lpthread"
 LIBPATH="-I/usr/include  -L/usr/local/lib"
 cd ../src
 
-gcc -g -lm -w -std=99 conver_spi.c spi.c bike_play.c -o ../run/bike_play $LIBPATH $LIB
+gcc -g -lm -w conver_spi.c spi.c bike_play.c -o ../run/bike_play $LIBPATH $LIB
 
  
